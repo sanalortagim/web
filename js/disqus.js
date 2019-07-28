d62d7b1fdb5c551b34b62c669f311620ccd781bc
sanalortagim <47482772+sanalortagim@users.noreply.github.com>
2019-07-28 22:28:46 +0300

    test

--- js/disqus.js
+++ js/disqus.js
@@ -1,14 +1,15 @@
-  var disqus_config = function () {
-    // we are using document.location.* here because Jekyll code does not work well in JS code.
-    this.page.url = document.location.href;
-    this.page.identifier = document.location.pathname;
-  };
-
-  // You should be able to get the following lines of code from your Disqus admin.
-  // https://disqus.com/admin/universalcode
-  (function() { // DON'T EDIT BELOW THIS LINE
-    var d = document, s = d.createElement('script');
-    s.src = '//computertechnologies.disqus.com/embed.js';
-    s.setAttribute('data-timestamp', +new Date());
-    (d.head || d.body).appendChild(s);
-  })();
+/**
+*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+/*
+var disqus_config = function () {
+this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+*/
+(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.src = 'https://computertechnologies.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
