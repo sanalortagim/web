355098aa3ff81b582baa97b79101e9df898cc3ca
Sanal Ortagim <47482772+sanalortagim@users.noreply.github.com>
2020-12-04 09:41:53 +0300

    news.

--- cdn/v2.js
+++
@@ -1,1 +0,0 @@
-!function(){"use strict";var e;navigator.userAgent.match(/IEMobile\/10\.0/)&&((e=document.createElement("style")).appendChild(document.createTextNode("@-ms-viewport{width:auto!important}")),document.head.appendChild(e))}();
